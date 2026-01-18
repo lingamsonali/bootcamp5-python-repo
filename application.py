5dc9636eb67ac4eec7975c2cc4ecc84268b1cf26
sonali <lingamsonali012002@gmail.com>
2026-01-18 23:43:11 +0530

    Initial Commit

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route("/")
+def home():
+    return "Elastic Beanstalk Python App is running!"
+
+if __name__ == "__main__":
+    application.run()
