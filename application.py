d22d6991a4929dab14a6e1afac301f3d70e1964e
sonali <lingamsonali012002@gmail.com>
2026-01-21 23:45:01 +0530

    sixth Commit

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# AWS EB looks for 'application' variable by default
+application = Flask(__name__)
+app = application 
 
 @app.route("/")
 def home():
+    # Flask automatically looks inside the /templates folder
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
